2d06d979abf9b783977b9ac2eb8c6a595550219c
LeeByeoungJin <67147940+LeeByeoungJin@users.noreply.github.com>
2020-07-29 15:25:03 +0900

    Update random-01.c

--- C/Non-overlapping Random Number/random-01.c
+++ C/Non-overlapping Random Number/random-01.c
@@ -1,17 +1,26 @@
 //Non-overlapping Random Number Example 
 //01-Pick whole array, whenever discovered overlapping random number.
-//From 0 to 15, pick 10 numbers
+//From 0 to 15, pick 15 numbers
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int main(void) {
-	const int array_Length = 10;
+	const int array_Length = 15;
 	int array[array_Length];
+	bool vaild = true;
 	srand((unsigned)time(NULL));
-	for (int i = 0; i < array_Length; i++) {
-		array[i] = rand() % 15;
-		printf("\n%d", array[i]);
-	}
+	
+	do{
+		for(int i = 0; i < array_Length; i++)
+			array[i] = rand() % 15;
+		vaild = true;
+		for(int i = 0; i < array_Length; i++)
+			for(int j = 0; j < i; j++)
+				if(array[i] == array[j]){
+					vaild = false;
+					break;
+				}
+	}while(!vaild);	
 }
